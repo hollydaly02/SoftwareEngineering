0d5dd6a3512dd0bf5564d0ce8fcf6ed6c036767a
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-10-04 12:21:57 +0100

    numbers from user input added into an array

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter mathematical expression here: ");
+        String mathExpression = sc.nextLine();
+        String numbers[] = mathExpression.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
+    }
+
+}

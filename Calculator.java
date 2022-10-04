3155bae0117c18550d7b29dba5278b3e22175024
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-10-04 12:57:18 +0100

    made a method

--- Calculator.java
+++ Calculator.java
@@ -6,8 +6,11 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter mathematical expression here: ");
         String mathExpression = sc.nextLine();
-        String numbers[] = mathExpression.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
-        String operators [] = mathExpression.replaceAll("[0-9]","").split(""); //array of operators passed in
+
+        public static String Calculator(String str) {
+            String numbers[] = str.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
+            String operators [] = str.replaceAll("[0-9]","").split(""); //array of operators passed in
+        }
     }
 
 }

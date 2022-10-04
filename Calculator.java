8449a31cb6947e0bfeaa8b239ea36f8737453a99
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-10-04 13:06:05 +0100

    added switch cases with each operand (+-*)

--- Calculator.java
+++ Calculator.java
@@ -6,11 +6,29 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter mathematical expression here: ");
         String mathExpression = sc.nextLine();
-
-        public static String Calculator(String str) {
-            String numbers[] = str.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
-            String operators [] = str.replaceAll("[0-9]","").split(""); //array of operators passed in
-        }
+        String answer = Calculator(mathExpression);
+        System.out.println(answer);
     }
+    
+    public static String Calculator(String str){
+        String numbers[] = str.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
+        String operators [] = str.replaceAll("[0-9]","").split(""); //array of operators passed in
 
+        int total = Integer.parseInt(numbers[0]);
+
+        for (int i=0; i<operators.length; i++) {
+            switch (operators[i]) {
+            case "+" :
+                total += Integer.parseInt(numbers[i+1]);
+                break;
+            case "-" :
+                total -= Integer.parseInt(numbers[i+1]);
+                break;
+            case "*" :
+                total *= Integer.parseInt(numbers[i+1]);
+                break;
+            }
+        }
+        return String.valueOf(total);
+    }
 }

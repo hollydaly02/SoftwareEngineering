67b6c37e84f3972b822485a330ffa1caa0ff39e1
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-10-10 14:34:16 +0100

    Further tests added

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -47,10 +47,10 @@ public class CalculatorTest {
 	}
 	
 	@Test
-	public void testAdditionwithMultiplication() { //doesnt follow the rules of BIMDAS/BODMAS
+	public void testAdditionwithMultiplication() { 
 		String question = "2+5*7";
 		String answer = Calculator.Calculator(question);
-		String expectedAnswer = "49";
+		String expectedAnswer = "37";
 		
 		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
 	}
@@ -59,7 +59,61 @@ public class CalculatorTest {
 	public void testSubtractionwithMultiplication() {
 		String question = "9-4*2";
 		String answer = Calculator.Calculator(question);
-		String expectedAnswer = "10";
+		String expectedAnswer = "1";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+	}
+	
+	@Test
+	public void testSubtractionAndAdditionwithMultiplication() {
+		// -+*
+		String question = "50-4+5*8";
+		String answer = Calculator.Calculator(question);
+		String expectedAnswer = "86";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		// *+-
+		question = "10*2+5-7";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "18"; //XXXX
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		// +-*
+		question = "6+4-5*77";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "-375";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		// *-+
+		question = "72*3-8+9";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "217";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		// +*-
+		question = "30+14*6-5";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "109";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		// +*-
+		question = "8+66*4-57";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "215";
+				
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+	}
+	
+	@Test
+	public void testExampleGiven() {
+		String question = "12435+34569-12345*10+50";
+		String answer = Calculator.Calculator(question);
+		String expectedAnswer = "-76396";
 		
 		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
 	}

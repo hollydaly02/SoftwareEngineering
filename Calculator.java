5b19a3fc19c84fdbfde17344cf767693a70a09be
hollydaly02 <98813546+hollydaly02@users.noreply.github.com>
2022-10-10 13:31:32 +0100

    Precedence by using stack

--- Calculator.java
+++ Calculator.java
@@ -1,36 +1,92 @@
+/* A Java program to evaluate a
+given expression where userInput
+are separated by space.
+*/
+import java.util.Stack;
 import java.util.Scanner;
 
 public class Calculator {
+    public static int evaluate(String expression) {
+        char[] userInput = expression.toCharArray();          // Converts string to sequence of characters
+
+        Stack<Integer> numbers = new Stack<Integer>();        // Stack for numbers
+        Stack<Character> operators = new Stack<Character>();  // Stack for operators
+
+        for (int i = 0; i < userInput.length; i++) {
+            if (userInput[i] == ' ')
+                continue;                                     // Ignore blank space
+
+            
+            if (userInput[i] >= '0' && userInput[i] <= '9') { // If character is a number, we push to the number stack
+                StringBuffer sbuf = new StringBuffer();
+
+                while (i < userInput.length &&                // If there is more than one digit in the number, we neeed to append the digit in the stack and push
+                        userInput[i] >= '0' &&
+                        userInput[i] <= '9')
+                    sbuf.append(userInput[i++]);              
+                numbers.push(Integer.parseInt(sbuf.toString()));
+
+                i--;                                          // Correcting the offset, points to the character next to digit but the for loop increments i as well            
+            }
+
+            else if (userInput[i] == '+' ||                   // If character is an operator (not including division)
+                     userInput[i] == '-' ||
+                     userInput[i] == '*' ) {
+
+                while (!operators.empty() &&                  // While the operator at the top of the stack has the same or greater precedence as the current operator
+                        hasPrecedence(userInput[i],operators.peek()))
+                    numbers.push(applyOp(operators.pop(),
+                                         numbers.pop(),
+                                         numbers.pop()));     // Apply the operator on the top of the stack to the top two elements in numbers stack
+                operators.push(userInput[i]);                 // Push current character to operators stack
+            }
+        }
+
+        while (!operators.empty())                            // Apply the remaining operators to their corresponding numbers
+            numbers.push(applyOp(operators.pop(),
+                                 numbers.pop(),
+                                 numbers.pop()));
+        
+        return numbers.pop();                                 // The character at the top of the numbers stack is our result, return result     
+    }
+
+    public static boolean hasPrecedence(char operator1, char operator2) { 
+        if ((operator1 == '*') &&
+                (operator2 == '+' || operator2 == '-'))       // If the second operator has a lower precedence than the first, we return false
+            return false;
+        else
+            return true;                                      // otherwise we return true, for higher precedence
+    }
+
+    
+    public static int applyOp(char operator, int b, int a) {  // applying the operand and returning its result
+        switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        }
+        return 0;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter mathematical expression here: "); //takes in user input
-        String mathExpression = sc.nextLine();
-        String answer = Calculator(mathExpression);
-        System.out.println(answer);
-    }
-
-    public static String Calculator(String str){
-        String numbers[] = str.replaceAll("[+*-]","").split(""); //array of only the numbers in the string
-        String operators [] = str.replaceAll("[0-9]","").split(""); //array of operators passed in
-
-        int total = Integer.parseInt(numbers[0]); //total is set to be the first number in the array
-
-        for (int i=0; i<operators.length; i++) { //for loop - iterates until no more operators are left
-            switch (operators[i]) {
-            case "+" : //addition
-                total += Integer.parseInt(numbers[i+1]); //i + 1 used to add the next number to the total
+        boolean quit = false;
+        while(quit==false) {                                  // While the user still wishes to use the calculator, take in their expression and return its answer
+        	System.out.println("Enter mathematical expression here or enter quit ");
+            String mathExpression = sc.nextLine();
+        	if (mathExpression.matches("quit")) {             // If the user inputs quit, the program will end
+                quit = true;
                 break;
-            case "-" : //subtraction
-                total -= Integer.parseInt(numbers[i+1]);
-                break;
-            case "*" : //multiplication
-                total *= Integer.parseInt(numbers[i+1]);
-                break;
-            default: //when no operators are passed in
-              total = total;
             }
+        	else {
+        	System.out.println(Calculator.evaluate(mathExpression));
+        	}
         }
-        return String.valueOf(total); //returning the final total as a String
+        System.out.println("Goodbye");
+        sc.close();
     }
 }

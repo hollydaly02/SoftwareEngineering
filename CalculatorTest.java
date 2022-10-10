aeda304bd78da2028c182d30f1bf6ab0f44f8eb5
mfatima <mfatima@tcd.ie>
2022-10-10 11:38:18 +0100

    test file and pom.xml added

---
+++ CalculatorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAddition() {
+		String question = "2+2";
+		String answer = Calculator.Calculator(question);
+		String expectedAnswer = "4";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+		
+		question = "5+7";
+		answer = Calculator.Calculator(question);
+		expectedAnswer = "12";
+		
+		assertEquals ("The expected answer must match the answer.", expectedAnswer, answer);
+	}
+	
+}
